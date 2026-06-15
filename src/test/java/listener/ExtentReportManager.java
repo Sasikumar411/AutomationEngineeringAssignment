3cb9c73c5ac0cc84648abe3cbb6fca908b94ab55
“Sasikumar” <sasikumar2300@gmail.com>
2026-06-15 10:51:28 +0530

    Made some changes on the extent reports

--- src/test/java/listener/ExtentReportManager.java
+++ src/test/java/listener/ExtentReportManager.java
@@ -8,6 +8,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -20,14 +22,15 @@ import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import tests.BaseTest;
 
-public class ExtentReportManager implements ITestListener {
-	public ExtentSparkReporter sparkReporter;
+public class ExtentReportManager implements ITestListener, ISuiteListener {
+	private static ExtentSparkReporter sparkReporter;
 	public static ExtentReports extent;
-	public ExtentTest test;
+	private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
 
-	String repName;
-
-	public void onStart(ITestContext testContext) {
+	private static String repName;
+	
+	@Override
+	public void onStart(ISuite testContext) {
 		
 		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
 		
@@ -40,63 +43,71 @@ public class ExtentReportManager implements ITestListener {
 		
 		if(extent==null)
 		{
-			extent = new ExtentReports();
-			extent.attachReporter(sparkReporter);
-			extent.setSystemInfo("Application", "Amazon");
-			extent.setSystemInfo("Module", "Admin");
-			extent.setSystemInfo("Sub Module", "Customers");
-			extent.setSystemInfo("User Name", System.getProperty("user.name"));
-			extent.setSystemInfo("Environment", "QA");
+			synchronized (ExtentReportManager.class)
 		
-			
-	    
-		String os = testContext.getCurrentXmlTest().getParameter("os");
-		if(os!=null)
-	    {
-			extent.setSystemInfo("Operating System", os);
-	    }
-		
-		String browser = testContext.getCurrentXmlTest().getParameter("browser");
-		if(os!=null)
-	    {
-			extent.setSystemInfo("Browser", browser);
-	    }
-		List<String> includedGroups = testContext.getCurrentXmlTest().getIncludedGroups();
-		if(!includedGroups.isEmpty()) {
-		extent.setSystemInfo("Groups", includedGroups.toString());
-		}
+			    {
+				 if(extent == null)
+				 
+				 {
+					extent = new ExtentReports();
+					extent.attachReporter(sparkReporter);
+					extent.setSystemInfo("Application", "Amazon");
+					extent.setSystemInfo("Module", "Admin");
+					extent.setSystemInfo("Sub Module", "Customers");
+					extent.setSystemInfo("User Name", System.getProperty("user.name"));
+					extent.setSystemInfo("Environment", "QA");
+					
+					String os = testContext.getXmlSuite().getParameter("os");
+					if(os!=null)
+					{
+						extent.setSystemInfo("Operating System", os);
+				    }
+					
+					String browser = testContext.getXmlSuite().getParameter("browser");
+					if(browser!=null)
+				    {
+						extent.setSystemInfo("Browser", browser);
+				    }
+				 }
+			    }
 		}
 	}
-
+				
+	@Override
 	public void onTestSuccess(ITestResult result) {
 	
-		test = extent.createTest(result.getTestClass().getRealClass().getSimpleName() + " - " +result.getMethod().getMethodName());
-		test.assignCategory(result.getMethod().getGroups());
-		test.log(Status.PASS,result.getMethod().getMethodName() +" successfully executed");
+		test.set(extent.createTest(result.getTestClass().getRealClass().getSimpleName() + " - " +result.getMethod().getMethodName()));
+		test.get().assignCategory(result.getMethod().getGroups());
+		test.get().log(Status.PASS,result.getMethod().getMethodName() +" successfully executed");
+		test.remove();
 		
 	}
-
+	
+	@Override
 	public void onTestFailure(ITestResult result) {
 
-	    test = extent.createTest(result.getTestClass().getRealClass().getSimpleName() + " - " +result.getMethod().getMethodName());
-
-	    test.assignCategory(result.getMethod().getGroups());
-
-	    test.log(Status.FAIL, result.getMethod().getMethodName() + " got failed");
-
-	    test.log(Status.INFO, result.getThrowable().getMessage());
+		test.set(extent.createTest(result.getTestClass().getRealClass().getSimpleName() + " - " +result.getMethod().getMethodName()));
+		test.get().assignCategory(result.getMethod().getGroups());
+	    test.get().log(Status.FAIL, result.getMethod().getMethodName() + " got failed");
+	    test.get().log(Status.INFO, result.getThrowable().getMessage());
+	    test.remove();
 	}
-
+	
+	@Override
 	public void onTestSkipped(ITestResult result) {
-		test = extent.createTest(result.getTestClass().getRealClass().getSimpleName() + " - " +result.getMethod().getMethodName());
-		test.assignCategory(result.getMethod().getGroups());
-		test.log(Status.SKIP, result.getMethod().getMethodName()+" got skipped");
-		test.log(Status.INFO, result.getThrowable().getMessage());
+		test.set(extent.createTest(result.getTestClass().getRealClass().getSimpleName() + " - " +result.getMethod().getMethodName()));
+		test.get().assignCategory(result.getMethod().getGroups());
+		test.get().log(Status.SKIP, result.getMethod().getMethodName()+" got skipped");
+		test.get().log(Status.INFO, result.getThrowable().getMessage());
+		test.remove();
 	}
-
-	public void onFinish(ITestContext testContext) {
+	
+	@Override
+	public void onFinish(ISuite testContext) {
 		
-		extent.flush();
+		if (extent != null) {
+			extent.flush();
+			}
 		
 		String pathOfExtentReport = System.getProperty("user.dir")+"//reports//"+ repName;
 		File extentReport = new File(pathOfExtentReport);
